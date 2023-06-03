2e336dbb1bafd0b2e05f314c8690d9d23057c3c7
quackandcheese <quackandcheeseart@gmail.com>
2023-06-03 00:17:47 -0500

    set up base project using unity package

---
+++ Assets/_PekingJamCardPack/Code/CardHolder.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.ComTypes;
+using UnboundLib.Cards;
+using UnityEngine;
+
+public class CardHolder : MonoBehaviour
+{
+    public List<GameObject> Cards;
+    public List<GameObject> HiddenCards;
+
+    internal void RegisterCards()
+    {
+        foreach (var Card in Cards)
+        {
+            CustomCard.RegisterUnityCard(Card, PekingJamCardPack.modInitials, Card.GetComponent<CardInfo>().cardName, true, null);
+        }
+        foreach (var Card in HiddenCards)
+        {
+            CustomCard.RegisterUnityCard(Card, PekingJamCardPack.modInitials, Card.GetComponent<CardInfo>().cardName, false, null);
+            ModdingUtils.Utils.Cards.instance.AddHiddenCard(Card.GetComponent<CardInfo>());
+        }
+    }
+}
---
+++ Assets/_PekingJamCardPack/Code/PekingJamCardPack.cs
@@ -0,0 +1,31 @@
+﻿using BepInEx;
+using HarmonyLib;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.ComTypes;
+using UnityEngine;
+
+[BepInDependency("com.willis.rounds.unbound")]
+[BepInDependency("pykess.rounds.plugins.moddingutils")]
+[BepInDependency("pykess.rounds.plugins.cardchoicespawnuniquecardpatch")]
+[BepInPlugin(MOD_ID, MOD_NAME, VERSION)]
+[BepInProcess("Rounds.exe")]
+public class PekingJamCardPack : BaseUnityPlugin
+{
+    public const string MOD_ID = "com.quackandcheese.rounds.pekingjamcardpack";
+    public const string MOD_NAME = "PekingJamCardPack";
+    public const string VERSION = "0.1.0";
+
+    internal static AssetBundle assets;
+
+    internal static string modInitials = "Peking Jam";
+
+    void Awake()
+    {
+        var harmony = new Harmony(MOD_ID);
+        harmony.PatchAll();
+        assets = Jotunn.Utils.AssetUtils.LoadAssetBundleFromResources("assets", typeof(PekingJamCardPack).Assembly);
+
+        //assets.LoadAsset<GameObject>("ModCards").GetComponent<CardHolder>().RegisterCards();
+    }
+}
